2b1b8152e009832ae001e4b0b12d19de1b634a74
lucas44d <lucas.durand44190@gmail.com>
2026-07-08 11:02:29 -0400

    adding OOP architecture to the projet and start of first version

--- main.py
+++ main.py
@@ -1,16 +1,33 @@
-import sys
+#import sys
 
-import numpy as np
-import pandas as pd
+#import numpy as np
+#import pandas as pd
 
-from scipy import interpolate
-from scipy import optimize
+#from scipy import interpolate
+#from scipy import optimize
 
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
-from openpyxl import load_workbook
+#from openpyxl import load_workbook
 
-from PySide6.QtWidgets import (
-    QApplication,
-    QMainWindow,
-)
+#from PySide6.QtWidgets import (
+#    QApplication,
+#    QMainWindow,
+# )
+
+from src.models.reactor import Reactor
+
+
+def main():
+
+    stomach = Reactor("Estomac")
+
+    intestine = Reactor("Intestin")
+
+    print(stomach)
+
+    print(intestine)
+
+
+if __name__ == "__main__":
+    main()
